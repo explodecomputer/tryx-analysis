7125abb494144cc4a6257619e6f1b1f9194965e8
Gibran Hemani <explodecomputer@gmail.com>
2019-05-07 02:32:09 +0100

    upd

---
+++ scripts/run_simulations5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=sim5
+#SBATCH --nodes=1
+#SBATCH --mem=10G
+#SBATCH --time=0-10:00:00
+#SBATCH --array=1-200
+#SBATCH --output=job_reports/slurm-%A_%a.out
+#SBATCH --partition=mrcieu
+
+echo "Running on ${HOSTNAME}"
+
+if [ -n "${1}" ]; then
+  echo "${1}"
+  SLURM_ARRAY_TASK_ID=${1}
+fi
+
+i=${SLURM_ARRAY_TASK_ID}
+
+Rscript run_simulations5.r ${i} 160 1
+
