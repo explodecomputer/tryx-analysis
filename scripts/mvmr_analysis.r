4b8f8083e2caa817f7f24ad924e2beb41e303826
Gibran Hemani <explodecomputer@gmail.com>
2019-08-08 13:33:05 +0100

    cleaned up files

--- scripts/mvmr_analysis.r
+++ scripts/mvmr_analysis.r
@@ -1,12 +1,16 @@
 library(tidyverse)
 
 load("../results/mvmr_simulations.rdata")
+m <- subset(m, !is.na(nprox))
 
 s <- m %>% group_by(method, sim, plei, nprox) %>%
 summarise(n=sum(b!=0), b=sum(b), model=ifelse(first(plei), "Pleiotropic", "Simple"))
 
-group_by(s, model, method, nprox) %>% summarise(est=mean(b), se=sd(b)/sqrt(n()), bias_perc=mean((b-0.4) / 0.4)*100, n_variables=mean(n), n_sims=n()) %>%
+tab <- group_by(s, model, method, nprox) %>% summarise(est=mean(b), se=sd(b)/sqrt(n()), bias_perc=mean((b-0.4) / 0.4)*100, n_variables=mean(n), n_sims=n()) 
+
+tab %>%
 knitr::kable()
 
+write.csv(tab, file="../results/mvmr_simulations_summary.csv")
 
 
